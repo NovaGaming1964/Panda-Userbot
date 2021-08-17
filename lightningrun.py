77a87940eb7cc5f4d9f63dd691c5042d74a2779a
NovaGaming1964 <40416745+NovaGaming1964@users.noreply.github.com>
2021-08-17 14:00:29 +0530

    Update lightningrun.py

--- lightningrun.py
+++ lightningrun.py
@@ -1,2 +1,2 @@
 import os
-os.system("git clone https://github.com/KeinShin/Black-Lightning /root/userbot && mkdir /root/userbot/bin/ && cd /root/userbot/ && chmod +x /usr/local/bin/* && python3 -m userbot")
+os.system("git clone https://github.com/NovaGaming1964/Panda-Userbot /root/userbot && mkdir /root/userbot/bin/ && cd /root/userbot/ && chmod +x /usr/local/bin/* && python3 -m userbot")
